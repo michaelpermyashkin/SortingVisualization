e2a9a4029fc6bcaf00e285655e719e900b35e760
Michael <mpermyashkin@gmail.com>
2020-05-06 18:11:04 -0400

    added bubble sort

--- sorter.js
+++ sorter.js
@@ -49,68 +49,69 @@ console.log('QUICKSORT: ', ...array_quicksort);
 /************* MergeSort ****************/
 function mergeSort(array) {
     if (array.length === 1) {
-        return array
+        return array;
     }
 
-    const middle = Math.floor(array.length / 2)
-    const left = array.slice(0, middle)
-    const right = array.slice(middle)
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
 
     return merge(
         mergeSort(left),
         mergeSort(right)
-    )
+    );
 }
 
 function merge(left, right) {
-    let result = []
-    let indexLeft = 0
-    let indexRight = 0
+    let result = [];
+    let indexLeft = 0;
+    let indexRight = 0;
 
     while (indexLeft < left.length && indexRight < right.length) {
         if (left[indexLeft] < right[indexRight]) {
-            result.push(left[indexLeft])
-            indexLeft++
+            result.push(left[indexLeft]);
+            indexLeft++;
         } else {
-            result.push(right[indexRight])
-            indexRight++
+            result.push(right[indexRight]);
+            indexRight++;
         }
     }
 
-    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
 }
-var array_mergesort = arr.slice()
-array_mergesort = mergeSort(array_mergesort)
-console.log('MERGESORT: ', ...array_mergesort)
+var array_mergesort = arr.slice();
+array_mergesort = mergeSort(array_mergesort);
+console.log('MERGESORT: ', ...array_mergesort);
 
 
 
 /************* HeapSort ****************/
-function heapify(array, size, root) {
-    let max = root
-    let left = 2 * root + 1
-    let right = 2 * root + 2
-    if (left < size && array[left] > array[max]) {
-        max = left
+function heapify(array, length, root) {
+    var max = root;
+    var left = 2 * root + 1;
+    var right = 2 * root + 2;
+    if (left < length && array[left] > array[max]) {
+        max = left;
     }
-    if (right < size && array[right] > array[max]) {
-        max = right
+    if (right < length && array[right] > array[max]) {
+        max = right;
     }
     if (max != root) {
-        swap(array, root, max)
-        heapify(array, size, max)
+        swap(array, root, max);
+        heapify(array, length, max);
     }
 }
 
 function heapSort(array) {
-    let size = array.length
+    var length = array.length;
 
-    for (let i = Math.floor(size / 2 - 1); i >= 0; i--)
-        heapify(array, size, i)
+    for (var i = Math.floor(length / 2 - 1); i >= 0; i--) {
+        heapify(array, length, i);
+    }
 
-    for (let i = size - 1; i >= 0; i--) {
-        swap(array, 0, i)
-        heapify(array, i, 0)
+    for (var i = length - 1; i >= 0; i--) {
+        swap(array, 0, i);
+        heapify(array, i, 0);
     }
     return array
 }
@@ -118,3 +119,24 @@ function heapSort(array) {
 var array_heapsort = arr.slice()
 array_heapsort = heapSort(array_heapsort)
 console.log('HEAPSORT:  ', ...array_heapsort)
+
+
+/************* BubbleSort ****************/
+function bubbleSort(array) {
+    var length = array.length;
+    var swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (var i = 0; i < len; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(array, i, i + 1);
+                swapped = true;
+            }
+        }
+    }
+    return array;
+};
+
+var array_bubblesort = arr.slice()
+array_bubblesort = heapSort(array_bubblesort)
+console.log('BUBBLESORT:', ...array_bubblesort)
